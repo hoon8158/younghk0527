7a280c096af8d24572861001a5dde10f97d541b0
hoon8158 <126794062+hoon8158@users.noreply.github.com>
2023-04-13 14:05:51 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,15 @@
+for i in range(1, 10) :
+    for n in range(2, 6) :
+        print(f'{n} x {i} ={n * i:2d}','\t' ,end = ' ')
+        n += 1
+    print(' ')
+    i += 1
+
+print()
+
+for i in range(1, 10) :
+    for n in range(6, 10) :
+        print(f'{n} x {i} ={n * i:2d}','\t' ,end = ' ')
+        n += 1
+    print(' ')
+    i += 1
